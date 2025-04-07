a0691cd044d4c40572a0d0fca19acc97a7823617
CoderKumarS <100927807+CoderKumarS@users.noreply.github.com>
2025-04-07 12:29:07 +0530

    error

--- routes/web.php
+++ routes/web.php
@@ -8,15 +8,12 @@ use App\Http\Controllers\ArtistController;
 
 Route::controller(HomeController::class)->group(function () {
     Route::get('/', 'index')->name('home');
-<<<<<<< HEAD
     Route::post('/gallery', 'gallery')->name('gallery');
     Route::get('/artist', 'artist_index')->name('artists.index');
     Route::post('/about', 'about')->name('about');
-=======
     Route::get('/gallery', 'gallery')->name('gallery');
     Route::get('/artists', 'artists')->name('artists');
     Route::get('/about', 'about')->name('about');
->>>>>>> 52b626ceed1435536922ef497103b330738c62cc
     Route::get('/contact', 'contact')->name('contact');
 });
 Route::controller(AuthController::class)->group(function () {
