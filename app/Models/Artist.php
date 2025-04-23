a121963ff95258a690dbdfd28cedf12e9c7c7c86
CoderKumarS <100927807+CoderKumarS@users.noreply.github.com>
2025-04-23 14:38:07 +0530

    Back to mysql

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
 class Artist extends Model
 {
     public function user()
--- config/cache.php
+++ config/cache.php
@@ -88,7 +88,7 @@ return [
 
         'octane' => [
             'driver' => 'octane',
-        ],
+        ]
 
     ],
 
@@ -103,6 +103,6 @@ return [
     |
     */
 
-    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_cache_'),
+    'prefix' => env('CACHE_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_cache_'),
 
 ];
